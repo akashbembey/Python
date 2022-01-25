0f3b39f4973c7b0815f7d266112957fc06be7550
Preeti Bembey <74343887+akashbembey@users.noreply.github.com>
2022-01-25 09:41:31 +0530

    Create Comparision operators.py

---
+++ Assignment 1.1/Comparision operators.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+if num1 > num2:
+    print(num1," is greator than ",num2)
+elif num2 > num1:
+    print(num2," is greator than ",num1)
+elif num1==num2:
+    print(num1,"(first number)"," and ",num2,"(second number)"," are same")
