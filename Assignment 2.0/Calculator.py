e4f929f2fea2551510739cb639a3b829da9648b6
Preeti Bembey <74343887+akashbembey@users.noreply.github.com>
2022-01-25 10:05:22 +0530

    Create Calculator.py

---
+++ Assignment 2.0/Calculator.py
@@ -0,0 +1,169 @@
+from time import sleep
+print("Enter\n1 for additon\n2 for subtraction\n3 for multiplication\n4 for division")
+userchoice = int(input())
+if userchoice==1:
+    print("Enter 2 numbers")
+    num1 = int(input())
+    num2 = int(input())
+    ans = num1+num2
+    sleep(1)
+    print(ans)
+elif userchoice==2:
+    print("Enter 2 numbers")
+    num1 = int(input())
+    num2 = int(input())
+    ans = num1-num2
+    sleep(1)
+    print(ans)
+elif userchoice==3:
+    print("Enter 2 numbers")
+    num1 = int(input())
+    num2 = int(input())
+    ans = num1*num2
+    sleep(1)
+    print(ans)
+elif userchoice==4:
+    print("Enter 2 numbers")
+    num1 = int(input())
+    num2 = int(input())
+    ans = num1/num2
+    sleep(1)
+    print(ans)
+else:
+    print("Wrong number")
+    print("Restarting...")
+    sleep(3)
+    print("Enter 1 for additon 2 for subtraction 3 for multiplication 4 for division")
+    userchoice = int(input())
+    if userchoice==1:
+        print("Enter 2 numbers")
+        num1 = int(input())
+        num2 = int(input())
+        ans = num1+num2
+        sleep(1)
+        print(ans)
+    elif userchoice==2:
+        print("Enter 2 numbers")
+        num1 = int(input())
+        num2 = int(input())
+        ans = num1-num2
+        sleep(1)
+        print(ans)
+    elif userchoice==3:
+        print("Enter 2 numbers")
+        num1 = int(input())
+        num2 = int(input())
+        ans = num1*num2
+        sleep(1)
+        print(ans)
+    elif userchoice==4:
+        print("Enter 2 numbers")
+        num1 = int(input())
+        num2 = int(input())
+        ans = num1/num2
+        sleep(1)
+        print(ans)
+    else:
+        print("Wrong number")
+    print("Restarting...")
+    sleep(3)
+    print("Enter 1 for additon 2 for subtraction 3 for multiplication 4 for division")
+    userchoice = int(input())
+    if userchoice==1:
+        print("Enter 2 numbers")
+        num1 = int(input())
+        num2 = int(input())
+        ans = num1+num2
+        sleep(1)
+        print(ans)
+    elif userchoice==2:
+        print("Enter 2 numbers")
+        num1 = int(input())
+        num2 = int(input())
+        ans = num1-num2
+        sleep(1)
+        print(ans)
+    elif userchoice==3:
+        print("Enter 2 numbers")
+        num1 = int(input())
+        num2 = int(input())
+        ans = num1*num2
+        sleep(1)
+        print(ans)
+    elif userchoice==4:
+        print("Enter 2 numbers")
+        num1 = int(input())
+        num2 = int(input())
+        ans = num1/num2
+        sleep(1)
+        print(ans)
+    else:
+        print("Wrong number")
+    print("Restarting...")
+    sleep(3)
+    print("Enter 1 for additon 2 for subtraction 3 for multiplication 4 for division")
+    userchoice = int(input())
+    if userchoice==1:
+        print("Enter 2 numbers")
+        num1 = int(input())
+        num2 = int(input())
+        ans = num1+num2
+        sleep(1)
+        print(ans)
+    elif userchoice==2:
+        print("Enter 2 numbers")
+        num1 = int(input())
+        num2 = int(input())
+        ans = num1-num2
+        sleep(1)
+        print(ans)
+    elif userchoice==3:
+        print("Enter 2 numbers")
+        num1 = int(input())
+        num2 = int(input())
+        ans = num1*num2
+        sleep(1)
+        print(ans)
+    elif userchoice==4:
+        print("Enter 2 numbers")
+        num1 = int(input())
+        num2 = int(input())
+        ans = num1/num2
+        sleep(1)
+        print(ans)
+    else:
+        print("Wrong number")
+    print("Restarting...")
+    sleep(3)
+    print("Enter 1 for additon 2 for subtraction 3 for multiplication 4 for division")
+    userchoice = int(input())
+    if userchoice==1:
+        print("Enter 2 numbers")
+        num1 = int(input())
+        num2 = int(input())
+        ans = num1+num2
+        sleep(1)
+        print(ans)
+    elif userchoice==2:
+        print("Enter 2 numbers")
+        num1 = int(input())
+        num2 = int(input())
+        ans = num1-num2
+        sleep(1)
+        print(ans)
+    elif userchoice==3:
+        print("Enter 2 numbers")
+        num1 = int(input())
+        num2 = int(input())
+        ans = num1*num2
+        sleep(1)
+        print(ans)
+    elif userchoice==4:
+        print("Enter 2 numbers")
+        num1 = int(input())
+        num2 = int(input())
+        ans = num1/num2
+        sleep(1)
+        print(ans)
+    else:
+        print("Wrong number cant restart automaticaly please restart manualy")
