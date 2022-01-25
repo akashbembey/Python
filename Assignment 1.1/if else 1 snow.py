cf6a9756f27df2f86cf7d3a686d73a945ec87b58
Preeti Bembey <74343887+akashbembey@users.noreply.github.com>
2022-01-25 09:59:24 +0530

    Add files via upload

---
+++ Assignment 1.1/if else 1 snow.py
@@ -0,0 +1,5 @@
+doesitsnows = int(input("If it snows press 1 and if it dosent press 0"))
+if doesitsnows==1:
+    print("I will go out")
+elif doesitsnows==0:
+    print("I won't go out")
---
+++ Assignment 1.1/if else 2 rain.py
@@ -0,0 +1,5 @@
+isitraining = int(input("Press 1 if it is raining and 0 if not"))
+if isitraining==1:
+    print("Frogs will dance")
+elif isitraining==0:
+    print("Frogs will not dance")
